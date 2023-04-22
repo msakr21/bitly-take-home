ec155138af63bb9e4466dc7303b31d1a58f8f9b7
Mostafa Sakr <msakr3@gmail.com>
2023-04-22 06:16:15 -0600

    Create gemfile with source, ruby version, and rspec; create encode class test and implementation code.

---
+++ spec/encode_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require './lib/encode.rb'
+
+RSpec.describe Encode do
+  let(:google) {Encode.new("www.google.com", "bit.ly/31Tt55y")}
+
+  it "exists" do
+    expect(google).to be_an(Encode)
+  end
+
+  it "has readable attributes 'long_link' and 'short_link'" do
+    expect(google.long_link).to eq("www.google.com")
+    expect(google.short_link).to eq("bit.ly/31Tt55y")
+  end
+end
